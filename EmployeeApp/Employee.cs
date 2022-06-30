e7707f8e99f59e8e14e1e411854e930d5d106a6b
Covali Andrei <krab4ek@gmail.com>
2022-06-30 20:25:15 +0300

    Add project files.

---
+++ EmployeeApp/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    internal class Employee
+    {
+        private string empName;
+        private int empID;
+        private float currPay;
+
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+        {
+            empName = name;
+            empID = id;
+            currPay = pay;
+        }
+
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine($"Name: {empName}");
+            Console.WriteLine($"ID: {empID}");
+            Console.WriteLine($"Pay: {currPay}");
+        }
+    }
+}
