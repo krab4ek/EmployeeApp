58ed010256336220ed90768917dc1b9f23c518c2
Covali Andrei <krab4ek@gmail.com>
2022-07-03 20:10:07 +0300

    add Hexagon clas

---
+++ EmployeeApp/Hexagon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    internal class Hexagon
+    {
+        public void Draw() => Console.WriteLine("Drawing as Hexagon!");
+    }
+}
--- EmployeeApp/Program.cs
+++ EmployeeApp/Program.cs
@@ -22,12 +22,68 @@ chucky.GiveBonus(300);
 chucky.DisplayStats();
 Console.WriteLine();
 
-var fran = new SalesPerson("Fran",23,567,6000,"888-000-999",31);
+var fran = new SalesPerson("Fran", 23, 567, 6000, "888-000-999", 31);
 fran.GiveBonus(400);
 fran.DisplayStats();
 
 BenefitPackage.BenefitPackageLevel myLevel = BenefitPackage.BenefitPackageLevel.Gold;
 
+Console.WriteLine();
+
+CastingExamples();
+
+Console.WriteLine();
+
+object frank = new Manager();
+Hexagon hex;
+try
+{
+    hex = (Hexagon)frank;
+}
+catch (InvalidCastException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+Console.WriteLine();
+
+object[] things = new object[4];
+things[0] = new Hexagon();
+things[1] = false;
+things[2] = new Manager();
+things[3] = "Last thing";
+
+foreach (object item in things)
+{
+    Hexagon h = item as Hexagon;
+    if (h == null)
+        Console.WriteLine("Item is not a hexagon");
+    else
+    {
+        h.Draw();
+    }
+}
+
+
+static void CastingExamples()
+{
+    object frank = new Manager("Frank Zappa", 45, 1, 32000, "1111-11-1111", 5);
+    GivePromotion((Manager)frank);
+    Console.WriteLine();
+
+    Employee moonUnit = new Manager("Pol Zwap", 56, 2, 44000, "22-222-2222", 12);
+    GivePromotion(moonUnit);
+    Console.WriteLine();
+
+    SalesPerson jill = new PTSalesPerson("Jill", 32, 5, 23000, "78-8000-0008", 9);
+    GivePromotion(jill);
+    Console.WriteLine();
+
+}
+
+static void GivePromotion(Employee emp)
+{
+    Console.WriteLine($"{emp.Name} was promoted!");
+}
 
 
 Console.ReadLine();
